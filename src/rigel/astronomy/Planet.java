c945b4d55aea9ab419a585a1984a7cac1f52e0b3
apinto <andrea.pinto@epfl.ch>
2020-03-09 11:56:29 +0100

    Update repo

--- src/rigel/astronomy/Planet.java
+++
@@ -1,9 +0,0 @@
-package rigel.astronomy;
-
-import rigel.coordinates.EquatorialCoordinates;
-
-public final class Planet extends CelestialObject {
-    public Planet(String name, EquatorialCoordinates equatorialPos, float angularSize, float magnitude) {
-        super(name, equatorialPos, angularSize, magnitude);
-    }
-}
