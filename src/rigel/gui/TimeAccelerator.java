57857c170044e81acb7fc0e31645107747759bb9
apinto <andrea.pinto@epfl.ch>
2020-04-20 14:33:04 +0200

    Upload time accelerator class

---
+++ src/rigel/gui/TimeAccelerator.java
@@ -0,0 +1,20 @@
+package rigel.gui;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
+@FunctionalInterface
+public interface TimeAccelerator {
+    ZonedDateTime adjust(ZonedDateTime startingSimulatedTime, long elapsedRealNs);
+
+    static TimeAccelerator continuous(int factor) {
+        return (start, elapsedNs) ->
+                start.plus(factor * elapsedNs, ChronoUnit.NANOS);
+    }
+
+    static TimeAccelerator discrete(Duration step, long stepsPerSecond) {
+        return (start, elapsedNs) ->
+                start.plus(step.multipliedBy(elapsedNs * stepsPerSecond / 1_000_000_000L));
+    }
+}
